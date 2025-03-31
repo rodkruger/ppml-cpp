0eb9f9477d551cc1ed7e43cf0ac27a75f549a777
Rodrigo Krüger <rodrigo.kruger.eng@gmail.com>
2025-03-31 16:22:42 -0300

    Change datasets experiment

--- src/examples/CkksPerceptron.cpp
+++ src/examples/CkksPerceptron.cpp
@@ -14,12 +14,12 @@ int main() {
 
     std::vector<std::unique_ptr<Dataset> > datasets;
     datasets.emplace_back(std::make_unique<BreastCancerDataset>(F11));
-    // datasets.emplace_back(std::make_unique<DiabetesDataset>(F11));
-    // datasets.emplace_back(std::make_unique<GliomaGradingDataset>(F11));
-    // datasets.emplace_back(std::make_unique<DifferentiatedThyroidDataset>(F11));
-    // datasets.emplace_back(std::make_unique<CirrhosisPatientDataset>(F11));
+    datasets.emplace_back(std::make_unique<DiabetesDataset>(F11));
+    datasets.emplace_back(std::make_unique<GliomaGradingDataset>(F11));
+    datasets.emplace_back(std::make_unique<DifferentiatedThyroidDataset>(F11));
+    datasets.emplace_back(std::make_unique<CirrhosisPatientDataset>(F11));
     // datasets.emplace_back(std::make_unique<LoanPredictionDataset>(F11));
-    // datasets.emplace_back(std::make_unique<CreditCardFraudDataset>(F11));
+    datasets.emplace_back(std::make_unique<CreditCardFraudDataset>(F11));
 
     for (auto &dataset: datasets) {
         CkksPerceptronExperiment("ckks_tanh_" + dataset->GetName(), *dataset, params).Run();
