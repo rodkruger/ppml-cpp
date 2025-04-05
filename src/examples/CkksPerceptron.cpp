d77c614fbc515041313d812d81797c08bb0a8d58
Rodrigo Krüger <rodrigo.kruger.eng@gmail.com>
2025-04-05 11:52:40 -0300

    Experiment for sigmoid and tanh

--- src/examples/CkksPerceptron.cpp
+++ src/examples/CkksPerceptron.cpp
@@ -7,11 +7,6 @@ int main() {
     constexpr auto epochs = 10;
     const auto epochs_str = std::to_string(epochs);
 
-    CkksPerceptronExperimentParams params{};
-    params.activation = CkksPerceptron::SIGMOID;
-    params.epochs = epochs;
-    params.earlyBootstrapping = 0;
-
     std::vector<std::unique_ptr<Dataset> > datasets;
     datasets.emplace_back(std::make_unique<BreastCancerDataset>(F11));
     datasets.emplace_back(std::make_unique<DiabetesDataset>(F11));
@@ -21,7 +16,20 @@ int main() {
     // datasets.emplace_back(std::make_unique<LoanPredictionDataset>(F11));
     // datasets.emplace_back(std::make_unique<CreditCardFraudDataset>(F11));
 
+    CkksPerceptronExperimentParams params{};
+    params.activation = CkksPerceptron::SIGMOID;
+    params.epochs = epochs;
+    params.earlyBootstrapping = 0;
+
     for (auto &dataset: datasets) {
         CkksPerceptronExperiment("ckks_sigmoid_" + dataset->GetName(), *dataset, params).Run();
+    }
+
+    params.activation = CkksPerceptron::TANH;
+    params.epochs = epochs;
+    params.earlyBootstrapping = 0;
+
+    for (auto &dataset: datasets) {
+        CkksPerceptronExperiment("ckks_tanh_" + dataset->GetName(), *dataset, params).Run();
     }
 }
